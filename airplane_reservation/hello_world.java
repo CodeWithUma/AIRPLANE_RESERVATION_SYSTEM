8a47f8f79dae1918222e344c8c39caa8cdcecd53
Uma Prasad Das <61022623+CodeWithUma@users.noreply.github.com>
2021-11-07 02:28:05 +0530

    updated code

--- airplane_reservation/hello_world.java
+++ airplane_reservation/hello_world.java
@@ -1,5 +1,9 @@
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hi! Welcome Uma's Airline Reservation System");
+        System.out.println("-----------------------------------------------");
+        System.out.println("HI! WELCOME TO UMA'S AIRLINE RESERVATION SYSTEM");
+        System.out.println("-----------------------------------------------");
+        System.out.println();
+        System.out.println("PLEASE CHOOSE ONE AMONG THE FOLLOWING OPTIONS:- ");
     }
 }
